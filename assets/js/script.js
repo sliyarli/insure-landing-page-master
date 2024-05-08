3bdb117681f4f1030b15bcc4e41da8f76335d94e
Şahin Əliyarlı <Aliyarlisahin1958@gmail.com>
2024-05-09 02:04:40 +0400

    header is done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,31 @@
-const mobileLinks = document.querySelector("#mobile-links");
-const collapseBtn = document.querySelector("#collapseBtn");
+document.addEventListener("DOMContentLoaded", () => {
+  const mobileLinks = document.querySelector("#mobile-links");
+  const collapseBtn = document.querySelector("#collapseBtn");
+  const btnImage = document.querySelector("#collapseBtn img");
+  const headerImg = document.querySelector(".header-image");
 
-collapseBtn.addEventListener("click", () => {
-  mobileLinks.style.height = mobileLinks.clientHeight ? 0 : "260px";
+  collapseBtn.addEventListener("click", () => {
+    const isClosed =
+      mobileLinks.style.height === "0px" || mobileLinks.style.height === "";
+    if (isClosed) {
+      const headerImgHeight = window.getComputedStyle(headerImg).height;
+      mobileLinks.style.height = headerImgHeight;
+      btnImage.src = "./images/icon-close.svg";
+    } else {
+      mobileLinks.style.height = "0px";
+      btnImage.src = "./images/icon-hamburger.svg";
+    }
+  });
+
+  window.addEventListener("resize", () => {
+    if (mobileLinks.style.height !== "0px" && mobileLinks.style.height !== "") {
+      const headerImgHeight = window.getComputedStyle(headerImg).height;
+      mobileLinks.style.height = headerImgHeight;
+    }
+
+    if (window.innerWidth > 991.98) {
+      mobileLinks.style.height = "0px";
+      btnImage.src = "./images/icon-hamburger.svg";
+    }
+  });
 });
